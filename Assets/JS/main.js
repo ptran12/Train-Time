0ab5c9b269154e2ed17d41e322133937604e9311
Phi Tran <phitran@Phis-MBP.attlocal.net>
2018-11-18 20:09:22 -0600

    got values to connect with firebase

--- Assets/JS/main.js
+++ Assets/JS/main.js
@@ -1,5 +1,4 @@
-// Initialize Firebase 
-
+//////Initialize Firebase//////////
 var config = {
     apiKey: "AIzaSyBzuq_hE0dqOidhdpiZpzSR-DuwKHa9T7Q",
     authDomain: "train-time-project-d3891.firebaseapp.com",
@@ -7,26 +6,23 @@ var config = {
     projectId: "train-time-project-d3891",
     storageBucket: "train-time-project-d3891.appspot.com",
     messagingSenderId: "1052127532330"
-};
-
+  };
 firebase.initializeApp(config);
 
-var database = firebase.database();
+var database = firebase.database();  
 
 //////////////////////////////////////////////////
 //////////////////////////////////////////////////
 
 $("#add-train-btn").on("click", function(event) {
-    event.preventDefualt();
-
+    event.preventDefault();
     ///USER INPUT///////
-
     var trainName = $("#train-name-input").val().trim();
-    var destination = $("#destination-name-input").val().trim();
+    var destination = $("#destination-input").val().trim();
     var firstTime = $("#train-time-input").val().trim();
-    var trainFrequency = $("#requency-input").val().trim();
+    var trainFrequency = $("#frequency-input").val().trim();
 
-    /////////////Object to hold train data//////////////
+    ///////////Object to hold train data//////////////
     var newT = {
         name: trainName,
         trainDes: destination,
@@ -34,7 +30,19 @@ $("#add-train-btn").on("click", function(event) {
         frequency: trainFrequency
     };
 
+    console.log(newT.name);
+    console.log(newT.trainDes);
+    console.log(newT.time);
+    console.log(newT.frequency);
+
     ///Upload train data to the Firebase Database/////
     database.ref().push(newT);
+
+    alert("New Train Time Added!");
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
 
 });
